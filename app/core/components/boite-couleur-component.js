562843e724a2822a339fdc36a82b58ff9e3bc412
Xavier CARL <xavier.carl@soprasteria.com>
2020-08-13 17:52:12 +0200

    notification de changement de couleur sur matrice

--- app/core/components/boite-couleur-component.js
+++ app/core/components/boite-couleur-component.js
@@ -1,13 +1,26 @@
 angular.module('myApp.components', [])
 	.component('boite', {
 		templateUrl: 'core/components/boite-couleur-component.html',
-		controller: BoiteController,
+		controller: ['couleurNotificationService', BoiteController],
+		controllerAs: 'vm',
 		bindings: {
-			couleur: '<'
+			couleur: '@',
+			x: '@',
+			y: '@',
 		}
 	});
 
-function BoiteController() {
+function BoiteController(couleurNotificationService) {
 	var ctrl = this;
 
+	ctrl.$onInit = function () {
+		console.log("creation de la boite", ctrl.x, ctrl.y, ctrl.couleur);
+		ctrl.couleurAffichee = "not initialized";
+		couleurNotificationService.subscribe(ctrl.x, ctrl.y, changerCouleur);
+	};
+
+	function changerCouleur(nouvelleCouleur) {
+		ctrl.couleurAffichee = nouvelleCouleur;
+		ctrl.cssvar = {'background-color': nouvelleCouleur};
+	}
 }
--- app/core/services/couleur-notification-service.js
+++ app/core/services/couleur-notification-service.js
@@ -3,14 +3,19 @@
 angular.module('myApp.services', [])
 	.service("couleurNotificationService", ['$http', '$q', 'webSocketService', function ($http, $q, webSocketService) {
 		var service = {
+			connexionAuTopic: connexionAuTopic,
 			changerCouleur: changerCouleur,
 			subscribe: subscribe
 		};
 
+		var maxX = 12;
+		var maxY = 12;
+		var listeSubscriber = Array.from({length: maxX}, () => Array.from({length: maxY}, () => "black"));;
+
 		function changerCouleur(nouvelleCouleur) {
 			console.log("changement demandé", nouvelleCouleur);
 			var defer = $q.defer();
-			$http.get("http://localhost:9999/couleur/activer/" + nouvelleCouleur)
+			$http.get("http://localhost:9999/couleur/activer/" + nouvelleCouleur + "/12/12")
 				.then(function (data) {
 					defer.resolve(data);
 				}, function (data) {
@@ -19,9 +24,22 @@ angular.module('myApp.services', [])
 			return defer.promise;
 		}
 
-		function subscribe(callbackOnMessage) {
+		function connexionAuTopic() {
 			console.log("abonnement demandé");
-			webSocketService.connexion('/topic/couleurs', callbackOnMessage);
+			webSocketService.connexion('/topic/couleurs', notifierBoiteEnfant);
+		}
+
+		function subscribe(x, y, callbackOnMessage) {
+			console.log("abonnement demandé", x, y);
+			listeSubscriber[x][y] = callbackOnMessage;
+		}
+
+		function notifierBoiteEnfant(message) {
+			var x = message.x;
+			var y = message.y;
+			var couleur = message.couleur;
+			console.log("notification de la boite", x, y, couleur);
+			listeSubscriber[x][y](couleur);
 		}
 
 
