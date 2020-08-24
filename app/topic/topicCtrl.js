46bacb9b8269bbfb5508d03cb73d683fdffedc3c
Xavier CARL <xavier.carl@soprasteria.com>
2020-08-24 16:23:25 +0200

    authentification minimum

--- app/topic/topicCtrl.js
+++ app/topic/topicCtrl.js
@@ -9,45 +9,84 @@ angular.module('myApp.topic', [])
 				controllerAs: 'vm'
 			});
 	}])
-	.controller('topicCtrl', ['topicNotificationService', "$scope", "$timeout", function (topicNotificationService, $scope, $timeout) {
+	.controller('topicCtrl', ['topicNotificationService', "$scope", "$timeout", "$http",
+		function (topicNotificationService, $scope, $timeout, $http) {
 
-		var vm = this;
+			var vm = this;
 
-		$scope.topic = "topic1";
-		$scope.auteur = "Bob";
-		$scope.message = "coucou";
+			$scope.topic = "topic1";
+			$scope.auteur = "Bob";
+			$scope.message = "coucou";
 
-		$scope.envoyerMessage = envoyerMessage;
-		$scope.abonnementAuTopic = abonnementAuTopic;
+			$scope.connected = false;
 
+			$scope.authenticated = "ko";
+			$scope.username = "toto";
+			$scope.password = "titi";
+			$scope.reponseAuthent = "";
 
-		function envoyerMessage(topic,message, auteur) {
-			$scope.couleur = "changement en cours";
-			var changerCouleur = topicNotificationService.envoyerMessage(topic,message,auteur);
-			changerCouleur.then(function (response) {
-				$scope.reponse = " [" + angular.toJson(response.data) + "]";
-				$timeout(function () {
-					$scope.reponse = "";
-				}, 5000);
-			});
-		}
+			$scope.envoyerMessage = envoyerMessage;
+			$scope.abonnementAuTopic = abonnementAuTopic;
+
+			$scope.authenticate = authenticate;
 
 
-		function abonnementAuTopic(topicName) {
-			topicNotificationService.connexionAuTopicAvecCallback(topicName, receptionMessage)
-				.then(function (){
-					$scope.connected = true;
-				})
-				.catch(function (error){
-					$scope.connected = false;
-					console.error(error);
+			function authenticate(user, pass) {
+				console.log("authentification");
+				$scope.authenticated = "wip";
+				$http({
+					method: 'POST',
+					url: '/api/login',
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+					transformRequest: function (obj) {
+						var str = [];
+						for (var p in obj)
+							str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+						return str.join("&");
+					},
+					data: {username: user, password: pass}
+				}).then(function (data) {
+					$scope.authenticated = "ok";
+					$scope.reponseAuthent = " [" + angular.toJson(data) + "]";
+					$timeout(function () {
+						$scope.reponseAuthent = "";
+					}, 5000);
+				}, function (data) {
+					$scope.authenticated = "ko";
+					$scope.reponseAuthent = " [" + angular.toJson(data) + "]";
+					$timeout(function () {
+						$scope.reponseAuthent = "";
+					}, 5000);
 				});
-		}
+			}
 
-		function receptionMessage(message){
-			$scope.messages += angular.toJson(message);
-		}
+			function envoyerMessage(topic, message, auteur) {
+				$scope.couleur = "changement en cours";
+				var changerCouleur = topicNotificationService.envoyerMessage(topic, message, auteur);
+				changerCouleur.then(function (response) {
+					$scope.reponse = " [" + angular.toJson(response.data) + "]";
+					$timeout(function () {
+						$scope.reponse = "";
+					}, 5000);
+				});
+			}
 
-	}])
+
+			function abonnementAuTopic(topicName) {
+				topicNotificationService.connexionAuTopicAvecCallback(topicName, receptionMessage)
+					.then(function () {
+						$scope.connected = true;
+					})
+					.catch(function (error) {
+						$scope.connected = false;
+						console.error(error);
+					});
+			}
+
+			function receptionMessage(message) {
+				$scope.messages += angular.toJson(message);
+			}
+
+		}])
 ;
 
